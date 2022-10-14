ada88da7fa1926c21afbc425e67816584f2f72bd
chaoNTUEE <115763094+chaoNTUEE@users.noreply.github.com>
2022-10-14 21:18:56 +0800

    Add files via upload

---
+++ solution2_pipeline_loop/tcl/script.tcl
@@ -0,0 +1,20 @@
+############################################################
+## This file is generated automatically by Vitis HLS.
+## Please DO NOT edit it.
+## Copyright 1986-2022 Xilinx, Inc. All Rights Reserved.
+############################################################
+open_project dct_prj
+set_top dct
+add_files dct.cpp
+add_files -tb out.golden.dat -cflags "-Wno-unknown-pragmas" -csimflags "-Wno-unknown-pragmas"
+add_files -tb in.dat -cflags "-Wno-unknown-pragmas" -csimflags "-Wno-unknown-pragmas"
+add_files -tb dct_test.cpp -cflags "-Wno-unknown-pragmas" -csimflags "-Wno-unknown-pragmas"
+open_solution "solution2" -flow_target vivado
+set_part {xcvu9p-flgb2104-1-e}
+create_clock -period 10 -name default
+config_compile -pipeline_loops 6
+#source "./dct_prj/solution2/directives.tcl"
+csim_design
+csynth_design
+cosim_design
+export_design -format ip_catalog
